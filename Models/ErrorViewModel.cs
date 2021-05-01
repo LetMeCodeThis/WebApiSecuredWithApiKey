b241643387224d25b5788b019a6a3268a165b5b6
LetMeCodeThis <letmecodethis@gmail.com>
2021-05-01 21:54:58 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApiSecuredWithApiKey.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
